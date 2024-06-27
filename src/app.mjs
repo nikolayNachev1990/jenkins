887684ad20375ae319595ab37f4c4b079cdb08e3
niki <niki@niki-zikvid>
2024-06-27 15:35:52 +0300

    CI CD Jenkins init

--- src/app.mjs
+++ src/app.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World 2024!');
+  res.send('Hello World 20242!');
 });
 
 export default app;
